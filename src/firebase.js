7b924a95c02d8001bc3aef33f0d1014166e149da
DecadentIpsum <decadentipsum@gmail.com>
2019-03-27 16:13:31 +0200

    Enable Authentication / Logging in / persisting Login

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
   apiKey: 'AIzaSyAKbup7RIqOswEZnoi9tgp4469BJdpllKY',
@@ -13,4 +14,4 @@ firebase.initializeApp(config);
 
 const db = firebase.firestore();
 
-export { db };
+export { db, firebase };
