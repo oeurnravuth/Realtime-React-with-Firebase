865a8427eefefaeba9beb56af981783083ba3652
DecadentIpsum <decadentipsum@gmail.com>
2019-03-28 09:02:32 +0200

    Refactor messages

--- src/Messages.js
+++ src/Messages.js
@@ -9,32 +9,43 @@ function Messages() {
       <div className="EndOfMessages">That's every message!</div>
 
       {messages.map((message, index) => {
-        return index === 0 ? (
-          <div>
-            <div className="Day">
-              <div className="DayLine" />
-              <div className="DayText">12/6/2018</div>
-              <div className="DayLine" />
-            </div>
-            <div className="Message with-avatar">
-              <div className="Avatar" />
-              <div className="Author">
-                <div>
-                  <span className="UserName">Ryan Florence </span>
-                  <span className="TimeStamp">3:37 PM</span>
-                </div>
-                <div className="MessageContent">{message.text}</div>
-              </div>
-            </div>
-          </div>
+        const previous = messages[index - 1];
+        const showDay = false;
+        const showAvatar = !previous || message.user.id !== previous.user.id;
+        return showAvatar ? (
+          <FirstmessageFromUser message={message} showDay={showDay} />
         ) : (
-          <div>
+          <div key={message.id}>
             <div className="Message no-avatar">
               <div className="MessageContent">{message.text}</div>
             </div>
           </div>
         );
       })}
+    </div>
+  );
+}
+
+function FirstmessageFromUser({ message, showDay }) {
+  return (
+    <div key={message.id}>
+      {showDay && (
+        <div className="Day">
+          <div className="DayLine" />
+          <div className="DayText">12/6/2018</div>
+          <div className="DayLine" />
+        </div>
+      )}
+      <div className="Message with-avatar">
+        <div className="Avatar" />
+        <div className="Author">
+          <div>
+            <span className="UserName">Ryan Florence </span>
+            <span className="TimeStamp">3:37 PM</span>
+          </div>
+          <div className="MessageContent">{message.text}</div>
+        </div>
+      </div>
     </div>
   );
 }
