19b679057b22d601ad5c962c9ffcfdb0f2200bba
DecadentIpsum <decadentipsum@gmail.com>
2019-03-28 07:19:44 +0200

    Error Handling

--- src/App.js
+++ src/App.js
@@ -17,15 +17,30 @@ function App() {
 }
 
 function Login() {
+  const [authError, setAuthError] = useState(null);
+
   const handleSignIn = async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    await firebase.auth().signInWithPopup(provider);
+    try {
+      await firebase.auth().signInWithPopup(provider);
+    } catch (error) {
+      setAuthError(error);
+    }
   };
 
   return (
     <div className="Login">
       <h1>Chat!</h1>
       <button onClick={handleSignIn}>Sign in with Google</button>
+      {authError && (
+        <div>
+          <p>Sorry, there was a problem</p>
+          <p>
+            <i>{authError.message}</i>
+          </p>
+          <p>Please try again</p>
+        </div>
+      )}
     </div>
   );
 }
