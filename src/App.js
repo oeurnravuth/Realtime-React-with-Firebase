7be23662f4fc9c907cb4e09609daf0ee1d5fc32d
DecadentIpsum <decadentipsum@gmail.com>
2019-03-28 07:38:39 +0200

    Refactor Messages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return user ? (
     <div className="App">
       <Nav user={user} />
-      <Channel />
+      <Channel user={user} />
     </div>
   ) : (
     <Login />
--- src/ChatInputBox.js
+++ src/ChatInputBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { db } from './firebase';
 
-function ChatInputBox() {
+function ChatInputBox({ user }) {
   return (
     <form
       onSubmit={event => {
@@ -11,6 +11,7 @@ function ChatInputBox() {
           .doc('random')
           .collection('messages')
           .add({
+            user: db.collection('users').doc(user.uid),
             text: value,
             createdAt: new Date(),
           });
