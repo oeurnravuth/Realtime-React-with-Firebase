e0a403147b67008cf82d5a52e9c34dc31bc678fb
DecadentIpsum <decadentipsum@gmail.com>
2019-04-01 10:28:28 +0300

    Verifying RTDB writes

--- src/firebase.js
+++ src/firebase.js
@@ -17,8 +17,42 @@ const db = firebase.firestore();
 const rtdb = firebase.database();
 
 export function setupPresence(user) {
-  rtdb.ref('.info/connected').on('value', snapshot => {
-    console.log(snapshot.val());
+  const isOfflineForRTDB = {
+    state: 'offline',
+    lastChanged: firebase.database.ServerValue.TIMESTAMP,
+  };
+
+  const isOnlineforRTDB = {
+    state: 'online',
+    lastChanged: firebase.database.ServerValue.TIMESTAMP,
+  };
+
+  const isOfflineForFirestore = {
+    state: 'offline',
+    lastChanged: firebase.firestore.FieldValue.serverTimestamp(),
+  };
+
+  const isOnlineforFirestore = {
+    state: 'online',
+    lastChanged: firebase.firestore.FieldValue.serverTimestamp(),
+  };
+
+  const rtdbRef = rtdb.ref(`/status/${user.uid}`);
+  const userDoc = db.doc(`users/${user.uid}`);
+
+  rtdb.ref('.info/connected').on('value', async snapshot => {
+    if (snapshot.val() === false) {
+      userDoc.update({
+        status: isOfflineForFirestore,
+      });
+      return;
+    }
+
+    await rtdbRef.onDisconnect().set(isOfflineForRTDB);
+    rtdbRef.set(isOnlineforRTDB);
+    userDoc.update({
+      status: isOnlineforFirestore,
+    });
   });
 }
 
